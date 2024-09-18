f2f32af6390037bc0b6d62edc416a3c2de20ab50
rqb-repository <nrqb786@gmail.com>
2024-09-18 23:03:07 +0530

    first commit

---
+++ variables.js
@@ -0,0 +1,16 @@
+// variables in  javascript 
+// let , const 
+// example for let
+let a,b,c;
+a = 1;
+b = 'A';
+c = "hello world";
+a = "raqeeb"
+console.log(a);
+// const varaible
+const num  = 12
+console.log(num)
+
+num = 14
+
+console.log(num)
